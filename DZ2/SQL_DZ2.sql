8dc6c621d7f946432aaac60100b74a6d2ad1b86a
ViktorSh <shaker111@yandex.ru>
2024-03-03 15:55:24 +0300

    commit folder DZ3 with files

--- DZ2/SQL_DZ2.sql
+++ DZ2/SQL_DZ2.sql
@@ -1,47 +1,47 @@
-create table if not exists Artist (
-	id_artist SERIAL primary key,
-	name VARCHAR(60) unique not NULL
+CREATE TABLE IF NOT EXISTS Artist (
+	id_artist SERIAL PRIMARY KEY,
+	name VARCHAR(60) UNIQUE NOT NULL
 );
 
-create table if not exists Genre (
-	id_genre SERIAL primary key,
-	name VARCHAR(50) unique not NULL
+CREATE TABLE IF NOT EXISTS Genre (
+	id_genre SERIAL PRIMARY KEY,
+	name VARCHAR(50) UNIQUE NOT NULL
 );
 
-create table if not exists Artists_genre (
-	id_artist INTEGER references Artist(id_artist),
-	id_genre INTEGER references Genre(id_genre),
-	constraint cpk_PG primary key (id_artist, id_genre)
+CREATE TABLE IF NOT EXISTS Artists_genre (
+	id_artist INTEGER REFERENCES Artist(id_artist),
+	id_genre INTEGER REFERENCES Genre(id_genre),
+	CONSTRAINT cpk_PG PRIMARY KEY (id_artist, id_genre)
 );
 
-create table if not exists Album (
-	id_album SERIAL primary key,
-	name VARCHAR(50) not null,
-	release_year DATE not null
+CREATE TABLE IF NOT EXISTS Album (
+	id_album SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	release_year DATE NOT NULL
 );
 
-create table if not exists Album_artist (
-	id_artist INTEGER references Artist(id_artist),
-	id_album INTEGER references Album(id_album),
-	constraint cpk_AA primary key (id_artist, id_album)
+CREATE TABLE IF NOT EXISTS Album_artist (
+	id_artist INTEGER REFERENCES Artist(id_artist),
+	id_album INTEGER REFERENCES Album(id_album),
+	CONSTRAINT cpk_AA PRIMARY KEY (id_artist, id_album)
 );
 
-create table if not exists Track (
-	id_track SERIAL primary key,
-	name VARCHAR(50) not null,
-	duratuion INTEGER not null,
-		check(duratuion between 1 and 7000),
-	id_album INTEGER references Album(id_album)
+CREATE TABLE IF NOT EXISTS Track (
+	id_track SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	duratuion INTEGER NOT NULL,
+		CHECK(duratuion BETWEEN 1 AND 7000),
+	id_album INTEGER REFERENCES Album(id_album)
 );
 
-create table if not exists Collection (
-	id_collect SERIAL primary key,
-	name VARCHAR(60) not null,
-	release_year DATE not null
+CREATE TABLE IF NOT EXISTS Collection (
+	id_collect SERIAL PRIMARY KEY,
+	name VARCHAR(60) NOT NULL,
+	release_year DATE NOT NULL
 );
 
-create table if not exists C_track (
-	id_collect INTEGER references Collection(id_collect),
-	id_track INTEGER references Track(id_track),
-	constraint cpk_CT primary key (id_collect, id_track)
+CREATE TABLE IF NOT EXISTS C_track (
+	id_collect INTEGER REFERENCES Collection(id_collect),
+	id_track INTEGER REFERENCES Track(id_track),
+	CONSTRAINT cpk_CT PRIMARY KEY (id_collect, id_track)
 );
